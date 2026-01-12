1f5959d744d630bed2fdd4edf6a5390106410d87
unknown <benoit.champlon79@gmail.com>
2026-01-12 11:59:55 +0100

    wip

--- src/com/mns/cda/banque/personne/Salarie.java
+++ src/com/mns/cda/banque/personne/Salarie.java
@@ -1,7 +1,27 @@
 package com.mns.cda.banque.personne;
 
+import com.mns.cda.banque.compte.Compte;
+
+import java.util.UUID;
+
 public class Salarie {
 
     private String password;
+
+    public Salarie(String password) {
+        this.password = password;
+
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public Compte accederCompte(String password, UUID numero) {
+        if (getPassword() != password) {
+            return null;
+        }
+        return
+    }
 
 }
