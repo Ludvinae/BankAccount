72d896bb08fa40dfa9083e581302f6e8ca33faac
unknown <benoit.champlon79@gmail.com>
2026-01-15 11:34:00 +0100

    fix(utils): fixed a bug in genererCode method when string length was less than 8

--- src/com/mns/cda/banque/Utils.java
+++ src/com/mns/cda/banque/Utils.java
@@ -2,6 +2,12 @@ package com.mns.cda.banque;
 
 public class Utils {
     public static String generateCode(String input) {
-        return Integer.toHexString(input.hashCode()).toUpperCase().substring(0, 8);
+        String hex = Integer.toHexString(input.hashCode()).toUpperCase();
+
+        if (hex.length() >= 8) {
+            return hex.substring(0, 8);
+        }
+
+        return String.format("%8s", hex).replace(' ', '0');
     }
 }
