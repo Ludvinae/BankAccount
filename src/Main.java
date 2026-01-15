76f2195492a492cbcd1e4de42cd43e09e2381237
unknown <benoit.champlon79@gmail.com>
2026-01-15 10:35:44 +0100

    feat(simulation): now chooses a director randomly in general population

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
+import com.mns.cda.banque.Ville;
+import com.mns.cda.banque.personne.Personne;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class Main {
+    public static void main(String[] args) {
+
+        //Ville metz = new Ville("Metz");
+        Ville.genererPersonnes();
+        ArrayList<Personne> eligibles = new ArrayList<>(Ville.personnes.values());
+        System.out.println(Ville.personnes);
+
+        // boucle for each to iterate through hashmap to get a random personne
+        //Ville.personnes.forEach((id, personne) -> );
+
+        int index = (int) (Math.random() * eligibles.size());
+        Personne fondateur = eligibles.remove(index);
+        fondateur.fonderBanque("Banque Populaire", "abcde");
+        System.out.println(Ville.banques.values());
+
+
+    }
 }
