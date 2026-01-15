3155b67c33f6dfd2234486c1615fe348b475a0c7
unknown <benoit.champlon79@gmail.com>
2026-01-15 09:39:27 +0100

    feat(comptes): plan epargne link back to compte courant to get depotAuto and cloturer methods to work properly

--- src/com/mns/cda/banque/compte/PlanEpargne.java
+++ src/com/mns/cda/banque/compte/PlanEpargne.java
@@ -1,10 +1,34 @@
 package com.mns.cda.banque.compte;
 
-public class PlanEpargne extends Compte{
+import com.mns.cda.banque.personne.Client;
+
+import java.time.LocalDate;
+
+public class PlanEpargne extends Compte implements IRetrait{
 
     public PlanEpargne(double montant) {
         super(montant);
     }
 
+    @Override
+    public boolean retirer(double montant) {
+        return false;
+    }
 
+    public void depotAuto(Client client, double montant) {
+        LocalDate now = LocalDate.now();
+        Compte compte = client.getCompte("courant");
+        if (now.getDayOfMonth() == 1) {
+            String message = client.transfert(compte, this, montant);
+            System.out.println(message);
+        }
+    }
+
+    public void cloturer(Client client) {
+        Compte compte = client.getCompte("courant");
+        double montant = this.consulterSolde();
+        compte.deposer(montant);
+        this.setSolde(0);
+        client.fermerComptes("plan");
+    }
 }
