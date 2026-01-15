43db0bf97d9c994aa8df490e6d6cce1be06826e8
unknown <benoit.champlon79@gmail.com>
2026-01-15 10:02:06 +0100

    feat(personne): basic directeur class logic

--- src/com/mns/cda/banque/personne/Directeur.java
+++ src/com/mns/cda/banque/personne/Directeur.java
@@ -1,4 +1,11 @@
 package com.mns.cda.banque.personne;
 
-public class Directeur {
+import com.mns.cda.banque.Banque;
+
+import java.util.UUID;
+
+public class Directeur extends Salarie{
+    public Directeur(String password, String banqueId) {
+        super(password, banqueId);
+    }
 }
