ff5b760832b7c2227a320b5d402317ce4a29d685
unknown <benoit.champlon79@gmail.com>
2026-01-15 09:25:27 +0100

    feat(banque): Banque now use String codes as key for mapping comptes and clients instances storage

--- src/com/mns/cda/banque/Banque.java
+++ src/com/mns/cda/banque/Banque.java
@@ -1,31 +1,50 @@
 package com.mns.cda.banque;
 
+import com.mns.cda.banque.compte.Compte;
 import com.mns.cda.banque.personne.Client;
 import com.mns.cda.banque.personne.Personne;
+import com.mns.cda.banque.personne.Salarie;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.*;
 
 public class Banque {
-    protected HashMap<UUID, Client> clients;
+    protected HashMap<String, Client> clients;
     protected List<Salarie> salaries;
+    protected HashMap<String, Compte> comptes;
     protected String nom;
-    protected Personne directeur;
+    protected Personne fondateur;
+    protected String banqueId;
 
-    public Banque(String nom, Personne directeur) {
-        clients = new HashMap<>();
-        salaries = new ArrayList<>();
+    public Banque(String nom, Personne fondateur) {
+        this.clients = new HashMap<>();
+        this.salaries = new ArrayList<>();
+        this.comptes = new HashMap<>();
+
         this.nom = nom;
-        this.directeur = directeur;
+        this.fondateur = fondateur;
+        this.banqueId = Utils.generateCode(nom);
+        Ville.addBanque(this.banqueId, this);
     }
 
     public String getNom() {
         return nom;
     }
 
-    public void ajouterClient(UUID numero, Client client) {
+    public String getBanqueId() {
+        return banqueId;
+    }
+
+    public HashMap<String, Compte> getComptes() {
+        return comptes;
+    }
+
+    @Override
+    public String toString() {
+        return this.nom + " : " + this.clients.size() + " clients, fondé par " + this.fondateur;
+    }
+
+
+    public void ajouterClient(String numero, Client client) {
         clients.put(numero, client);
     }
 }
