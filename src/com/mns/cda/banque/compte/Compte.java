0bf87bde17e590f0f7331b38212892b75c091b63
unknown <benoit.champlon79@gmail.com>
2026-01-15 10:45:38 +0100

    feat(compte): creating a new account now also build an associated iban, changed Compte constructor to allow this behavior

--- src/com/mns/cda/banque/compte/Compte.java
+++ src/com/mns/cda/banque/compte/Compte.java
@@ -5,10 +5,15 @@ import java.time.LocalDate;
 public abstract class Compte {
     private LocalDate dateOuverture;
     private double solde;
+    private Iban iban;
+    protected Integer compteur = 0;
 
-    public Compte(double montant) {
+    public Compte(double montant, String banqueId, String codeClient) {
         this.solde = montant;
         this.dateOuverture = LocalDate.now();
+        compteur += 1;
+        String compteId = compteur.toString();
+        this.iban = new Iban(banqueId, codeClient, "generic", compteId);
     }
 
     public double consulterSolde() {
